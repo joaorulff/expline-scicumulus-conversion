6c8e1dd12a156b95f62ab60d15cf041e26a9eea7
dinhujr <c.souza0804@gmail.com>
2014-11-04 09:13:10 -0200

    create the logical method

--- src/br/ufrj/cos/expline/main/Main.java
+++ src/br/ufrj/cos/expline/main/Main.java
@@ -1,9 +1,14 @@
 package br.ufrj.cos.expline.main;
 
+import br.ufrj.cos.expline.scicumulus.convertion.Convertion_reader;
+import br.ufrj.cos.expline.scicumulus.convertion.Convertion_writer;
+import br.ufrj.cos.expline.scicumulus.convertion.IWriter;
+
 public class Main 
 {
 	public static void main(String [] args)
 	{
-		new ConverterReader();
+		IWriter writer = new Convertion_writer();
+		new Convertion_reader(writer);
 	}
 }
--- src/br/ufrj/cos/expline/scicumulus/convertion/Convertion_writer.java
+++ src/br/ufrj/cos/expline/scicumulus/convertion/Convertion_writer.java
@@ -1,7 +1,28 @@
 package br.ufrj.cos.expline.scicumulus.convertion;
 
-public class Convertion_writer {
+import javax.xml.soap.Node;
+
+public class Convertion_writer implements IWriter{
+
+	@Override
+	public void insertActivity(Node parent, Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertRelation(Node parent, Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertField(Node parent, Node node) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 
 }
+	
