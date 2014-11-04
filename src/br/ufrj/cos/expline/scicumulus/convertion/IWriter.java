a5a84afa0329876aa9a72dfb08c54f33394c1fce
João Lucas Rulff da Costa <jlrulff@gmail.com>
2014-11-04 08:09:41 -0200

    Document instantiation in Convertion_writer

--- src/br/ufrj/cos/expline/scicumulus/convertion/IWriter.java
+++ src/br/ufrj/cos/expline/scicumulus/convertion/IWriter.java
@@ -2,7 +2,7 @@ package br.ufrj.cos.expline.scicumulus.convertion;
 
 import javax.xml.soap.Node;
 
-public interface iReader 
+public interface IWriter 
 {
 	public void insertActivity( Node parent, Node node );
 	public void insertRelation( Node parent, Node node );
