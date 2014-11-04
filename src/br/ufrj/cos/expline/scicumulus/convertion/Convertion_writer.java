3b7875673440a388fd8a80c2fbb62359f4f9fdaa
João Lucas Rulff da Costa <jlrulff@gmail.com>
2014-11-04 08:16:25 -0200

    creation of scicumulusMainNodeCreation method

--- src/br/ufrj/cos/expline/scicumulus/convertion/Convertion_writer.java
+++ src/br/ufrj/cos/expline/scicumulus/convertion/Convertion_writer.java
@@ -1,7 +1,54 @@
 package br.ufrj.cos.expline.scicumulus.convertion;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class Convertion_writer {
 	
+	
+	Document scicumulusXML;	
+	
+	
+	public Convertion_writer(){
+		
+		try{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		this.scicumulusXML = builder.newDocument();
+		}catch(Exception e){
+			System.out.println("Error while creating the XML document");
+		}
+	}
+	
+	
+	public void scicumulusMainNodeCreation(){
+		Element root = this.scicumulusXML.createElement("SciCumulus");
+		this.scicumulusXML.appendChild(root);
+	}
+	
+	
+	public void saveDocumentToDisk(){
+		
+		try{
+		TransformerFactory transformerfactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerfactory.newTransformer();
+		DOMSource documentSource = new DOMSource(this.scicumulusXML);
+		StreamResult result = new StreamResult(new File("newDocument.xml"));
+		transformer.transform(documentSource, result);
+		System.out.println("File Saved");
+		}catch(Exception e){
+			System.out.println("Error while saving the XML file");
+		}
+	}
 	
 
 }
